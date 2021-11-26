d6899673cdc2c5b1178e3e3d5e4b98e4d21cacb3
FyodorovMS <spoon03@mail.ru>
2021-11-26 13:12:44 +0300

    configurate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classres, files
-# , tool windows, actions, and settings.
+# Press Double Shift to searcfh everywhere for classres, files
+# , tool windows, acfvfdvftions, and settings.
 
 
 def print_hi(name):
