ca6c1d7cf99494cf30f0b8fb6f55f7f39e67b0ec
FyodorovMS <spoon03@mail.ru>
2021-11-26 13:21:59 +0300

    create progect structure

---
+++ README.md
@@ -0,0 +1,31 @@
+[![Build Status](https://app.travis-ci.com/berpress/moodle_ui_test.svg?branch=main)](https://app.travis-ci.com/berpress/moodle_ui_test)
+# moodle_ui_test
+Selenium/Python
+
+
+Use python 3.8 +
+Create and activate virtual environments
+
+```
+python3 -m venv env
+source env/bin/activate
+```
+
+Run in terminal
+
+```
+pip install -r requirements.txt
+```
+
+pre-commit https://pre-commit.com
+```
+pre-commit run --all-files
+```
+
+Test app
+```
+https://qacoursemoodle.innopolis.university
+```
+
+
+pytest --alluredir=allure-results/
---
+++ conftest.py
@@ -0,0 +1,87 @@
+import logging
+import allure
+
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+
+from fixtures.models.login import LoginData
+from fixtures.pages.application import Application
+
+logger = logging.getLogger("moodle")
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--url",
+        action="store",
+        default="https://qacoursemoodle.innopolis.university/login/index.php",
+        help="Moodle url",
+    ),
+    parser.addoption(
+        "--username",
+        action="store",
+        default="super_qa_2021",
+        help="username",
+    ),
+    parser.addoption(
+        "--password",
+        action="store",
+        default="Password11!",
+        help="Password",
+    ),
+    parser.addoption(
+        "--headless",
+        action="store",
+        default="false",
+        help="enter 'true' if you want run tests in headless mode of browser,\n"
+        "enter 'false' - if not",
+    ),
+
+
+@pytest.fixture(scope="session")
+def user_data(request):
+    user = request.config.getoption("--username")
+    password = request.config.getoption("--password")
+    return LoginData(user, password)
+
+
+@pytest.fixture()
+def app(request):
+    url = request.config.getoption("--url")
+    headless = request.config.getoption("--headless")
+
+    # Опции  драйвера
+    chrome_options = Options()
+    if headless == "false":
+        chrome_options.headless = False
+    else:
+        chrome_options.headless = True
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    logger.info(f"Start app on {url}")
+    app = Application(driver, url)
+    yield app
+    app.quit()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed:
+        try:
+            if "app" in item.fixturenames:
+                web_driver = item.funcargs["app"]
+            else:
+                logger.error("Fail to take screen-shot")
+                return
+            logger.info("Screen-shot done")
+            allure.attach(
+                web_driver.driver.get_screenshot_as_png(),
+                name="screenshot",
+                attachment_type=allure.attachment_type.PNG,
+            )
+        except Exception as e:
+            logger.error("Fail to take screen-shot: {}".format(e))
