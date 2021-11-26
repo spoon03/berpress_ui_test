465efa519abeac7d4d4ffef4d05894b131e33540
FyodorovMS <spoon03@mail.ru>
2021-11-27 01:04:46 +0300

    Add test descriptions

---
+++ fixtures/pages/base_page.py
@@ -0,0 +1,37 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
+    def __init__(self, app):
+        self.app = app
+
+    def custom_find_element(self, locator, wait_time=60):
+        element = WebDriverWait(self.app.driver, wait_time).until(
+            EC.presence_of_element_located(locator),
+            message=f"Can't find element by locator {locator}",
+        )
+        return element
+
+    def click_element(self, locator, wait_time=60):
+        """
+        click_element = click
+        """
+        element = self.custom_find_element(locator, wait_time)
+        element.click()
+
+    def fill_element(self, data, locator, wait_time=60):
+        """
+        Fill element (fill_element == send_keys)
+        :param data: string to fill
+        """
+        element = self.custom_find_element(locator, wait_time)
+        if data:
+            element.send_keys(data)
+
+    def get_text(self, locator, wait_time=60) -> str:
+        """
+        Get element text
+        """
+        element = self.custom_find_element(locator, wait_time)
+        return element.text
---
+++ tests/test_lending.py
@@ -0,0 +1,38 @@
+class TestLending:
+    def test_search_positive(self, app):
+        """
+        Шаги:
+        1.Открыть страницу витрины
+        2.В поле поиска ввести имеющийся товар
+        3.Нажать кноаку Искать
+        3.Убедиться что товар отбразился
+        :param app:
+        :return:
+        """
+        app.open_lending_page()
+        assert 1 == 1
+
+    def test_search_negative(self, app):
+        """
+        Шаги:
+        1.Открыть страницу витрины
+        2.В поле поиска ввести отсутствующий товар
+        3.Нажать кноаку Искать
+        3.Убедиться что товар НЕ отбразился
+        :param app:
+        :return:
+        """
+        app.open_lending_page()
+        assert 1 == 1
+
+    def test_add_to_cart(self, app):
+        """
+        Шаги:
+        1.Открыть страницу поиска
+        2.Добавить товар в корзину
+        3.Убедиться что товар добавлен
+        :param app:
+        :return:
+        """
+        app.open_lending_page()
+        assert 1 == 1
